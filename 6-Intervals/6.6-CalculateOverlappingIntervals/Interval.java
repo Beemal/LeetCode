6cb3fb447e5a97c729b02448dbff2b9b7abea508
Julia Chen Jing <juliachenjing@gmail.com>
2017-01-04 20:36:30 -0600

    Update Interval.java

--- 6-Intervals/6.6-CalculateOverlappingIntervals/Interval.java
+++ 6-Intervals/6.6-CalculateOverlappingIntervals/Interval.java
@@ -1,9 +1,11 @@
+package testProject;
+
 class Interval{
     int start;
     int end;
-    Interval(int a,int b)//构造函数
-    {
-        start=a;
-        end=b;
-    }
+  
+	public Interval(int a, int b) {
+		  start=a;
+	        end=b;
+	}
 }
